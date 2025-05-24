8c8d36106352b5bddad5389e6ecd68e877685ad6
Taslak <158111299+adenisthebestaslan@users.noreply.github.com>
2025-05-24 09:59:24 -0500

    Update lookuptable.py

--- videochip/lookuptable.py
+++ videochip/lookuptable.py
@@ -1,13 +1,19 @@
 def SetcolourGraphicsDrawing(colour):
-     #later when we hit 100 colours, we will add headers, like if colour > 10, so we can look up the colour.
-    if colour == 1:
-        colour = (255, 0, 0)
+    #  later when we hit 100 colours, we will add headers, like if colour > 10, so we can look up the colour.
+    rgb = None
+    if colour == 10:
+        rgb = (255, 0, 0)
     elif colour == 20:
-        colour = (0, 255, 0)
+        rgb = (0, 255, 0)
     elif colour == 30:
-        colour = (0, 0, 255)
+        rgb = (0, 0, 255)
     elif colour == 0:
-        colour = (255, 255, 255)
+        rgb = (255, 255, 255)
+        print("Colour 0 is white")
+    else:
+        rgb = (0, 0, 0)  # Default to black or handle as needed
+    print(f"Colour set to: {rgb}")
+    return rgb
     
 
-
+ 
