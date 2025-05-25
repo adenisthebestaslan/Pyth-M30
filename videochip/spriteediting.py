1c7ae7c5f88490eb8c7673216c7beb6a65855433
Taslak <158111299+adenisthebestaslan@users.noreply.github.com>
2025-05-24 21:38:39 -0500

    Update and rename spriteediting to spriteediting.py

--- videochip/spriteediting.py
+++ videochip/spriteediting.py
@@ -3,7 +3,9 @@ import spritedrawer
 from copy import deepcopy
 def StrechSprite(SpriteEditing, Times):
     SpriteEdit = spritedrawer.GetrownumbersSpritedrawer(SpriteEditing)
+    print("sprite edit:", SpriteEdit)
     tmp = len(SpriteEdit[0])
+    print("tmp:", tmp)
     k = 1
     for row in SpriteEdit:
         tmp_list = deepcopy(row)
@@ -26,11 +28,25 @@ def StrechSprite(SpriteEditing, Times):
         with open(SpriteEditing, 'a') as file:
             file.write(','.join(row) + '\n')
 def size(SpriteEditing, Times):
-    StrechSprite("videochip/test.txt",10)
+    StrechSprite("videochip/test.txt",Times)
     # SpriteEditing: 
     SpriteEdit = spritedrawer.GetrownumbersSpritedrawer(SpriteEditing)
-     
+    # Only duplicate lists (rows) inside SpriteEdit, not elements within rows
+    new_SpriteEdit = []
+    for row in SpriteEdit:
+        for i in range(Times):
+            new_SpriteEdit.append(deepcopy(row))
+    SpriteEdit[:] = new_SpriteEdit  # Replace original with duplicated rows
+
+    # Save the modified SpriteEdit back to the file
+    open(SpriteEditing, 'w').close()  # Clear the file before writing
+    for row in SpriteEdit:
+        with open(SpriteEditing, 'a') as file:
+            file.write(','.join(row) + '\n')
+
+        
+
     # first we will take each row and take each item ex, 10,20, and add however many more times we want to add: ex: 10,10,10 becomes 10,10,10,10,10,10,10,10,10 if we were to size it by 3.
     #aftewards, we'll take each row and add a new item each time.
 
-size("videochip/test.txt",1)
+size("videochip/test.txt",10)
